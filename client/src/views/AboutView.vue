f33a6309f00040b5f64713fd508545e356382d10
mayaif <margareta.ifrim@outlook.com>
2024-09-26 20:22:53 +0100

    update ui

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,15 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="max-w-2xl">
+      <img src="../assets/images/coming_soon_img.png" />
+    </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
